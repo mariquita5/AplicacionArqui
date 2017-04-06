8c78f9eb361ee7c744848f7747722d51559821fb
Joseph <j-vv01@hotmail.com>
2017-04-06 17:46:54 -0600

    A veeeerrrr

--- App/public_html/Login.js
+++ App/public_html/Login.js
@@ -1,18 +1,3 @@
-window.onload=function() {
-  var un = document.getElementById("userName");
-	var pwd = document.getElementById("userPwd");
-	un.value= "Username";
-	pwd.value= "Password";
-	un.onfocus = function() {
-	  un.value = '';
-	};
-	un.onblur = function() {
-	  un.value = 'Username';
-	};
-	pwd.onfocus = function() {
-	  pwd.value = '';
-	};
-	pwd.onblur = function() {
-	  pwd.value = 'Password';
-	};
-};
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
